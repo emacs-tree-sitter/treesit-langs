c7933bd2868804819441f6e0f35a60a5d08a6198
JenChieh <jcs090218@gmail.com>
2025-02-27 11:38:10 -0800

    chore: Update copyright info

--- treesit-langs.el
+++ treesit-langs.el
@@ -1,6 +1,6 @@
 ;;; treesit-langs.el --- Language bundle for Emacs's treesit.el  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2024-2025  Shen, Jen-Chieh
+;; Copyright (C) 2024-2025  emacs-tree-sitter maintainers
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
