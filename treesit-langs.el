6d81817e85a49869e61b89399370fb801f3cebfb
Jen-Chieh Shen <jcs090218@gmail.com>
2025-09-14 19:22:54 -0700

    Bump bundle version

--- treesit-langs.el
+++ treesit-langs.el
@@ -38,7 +38,7 @@
   "Grammar bundle for `treesit.el'."
   :group 'tree-sitter)
 
-(defcustom treesit-langs-bundle-version "0.12.303"
+(defcustom treesit-langs-bundle-version "0.12.305"
   "Version of the grammar bundle.
 
 Ideally, we want this value to be same as `tree-sitter-langs--bundle-version'
