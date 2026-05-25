d7506e88c356aa202df5cb1c29c29f48e140531a
jcs090218 <8685505+jcs090218@users.noreply.github.com>
2026-05-25 20:18:43 +0000

    Update Bundle Version to 0.13.55

--- treesit-langs.el
+++ treesit-langs.el
@@ -38,7 +38,7 @@
   "Grammar bundle for `treesit.el'."
   :group 'tree-sitter)
 
-(defcustom treesit-langs-bundle-version "0.13.54"
+(defcustom treesit-langs-bundle-version "0.13.55"
   "Version of the grammar bundle.
 
 Ideally, we want this value to be same as `tree-sitter-langs--bundle-version'
